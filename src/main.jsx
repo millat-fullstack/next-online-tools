32ca260a3167e2b38da0651f6d8f3c6e49457511
millat-fullstack <fahimahm.millat@gmail.com>
2026-04-25 14:57:54 +0600

    Fix router basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/next-online-tools">
       <App />
     </BrowserRouter>
   </React.StrictMode>
